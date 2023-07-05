68c4916e987db63349c3ee07f370811a6b9c232f
Muhammad Saroosh Asif <sarooshasif77@gmail.com>
2023-07-05 17:55:35 +0200

    Update ethernet_script_send.py

--- Demo/scripts/ethernet_script_send.py
+++ Demo/scripts/ethernet_script_send.py
@@ -2,15 +2,15 @@ from time import sleep
 
 custom_mac = "6C:3C:8C:12:0D:17"
 
-
 ethernet_msg_1.mac_address_source = custom_mac
 ethernet_msg_1.mac_address_destination = Logging.get_mac()
-print(Logging.get_mac())
 ethernet_msg_1.payload = System.Array[Byte](bytearray.fromhex("01 02 03 04 09"))
 
-while(True):
-    
+count=20
+
+while(count>0):
     ethernet_msg_1.send()
+    count=count-1
     sleep(1)
 
 
